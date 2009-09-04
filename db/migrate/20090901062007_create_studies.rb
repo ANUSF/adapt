666e811c117fbf25d92314e3b34800168e3d28a4
Olaf Delgado-Friedrichs <olaf.delgado@gmail.com>
2009-09-04 12:53:23 +1000

    Fixed spelling in studies table field.

--- db/migrate/20090901062007_create_studies.rb
+++ db/migrate/20090901062007_create_studies.rb
@@ -2,7 +2,7 @@ class CreateStudies < ActiveRecord::Migration
   def self.up
     create_table :studies do |t|
       t.integer :user_id
-      t.string :permanent_identifyer
+      t.string :permanent_identifier
       t.string :name
       t.string :status
       t.text :ddi
