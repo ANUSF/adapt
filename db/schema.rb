9c5a3c296d01a5bf3be2c33d44bfdb4ae4252b8b
Olaf Delgado-Friedrichs <olaf.delgado@gmail.com>
2009-09-04 12:53:23 +1000

    Fixed spelling in studies table field.

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@ ActiveRecord::Schema.define(:version => 20090901062007) do
 
   create_table "studies", :force => true do |t|
     t.integer  "user_id",                             :null => false
-    t.string   "permanent_identifyer",                :null => false
+    t.string   "permanent_identifier",                :null => false
     t.string   "name",                                :null => false
     t.string   "status",                              :null => false
     t.string   "ddi",                  :limit => nil, :null => false
