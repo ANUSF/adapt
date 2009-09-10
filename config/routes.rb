617320ce8e11edd98e41a0097932bea0d1c2201b
Olaf Delgado-Friedrichs <olaf.delgado@gmail.com>
2009-09-10 12:46:24 +1000

    Nested study resource under user.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :studies
-
-  map.resources :studies
-
   map.resources :user_sessions
-  map.resources :users
+  map.resources :users, :has_many => :studies
 
   map.login "login", :controller => 'user_sessions', :action => 'new'
   map.logout "logout", :controller => 'user_sessions', :action => 'destroy'
