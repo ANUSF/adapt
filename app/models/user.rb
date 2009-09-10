9388969e64093098996c27abc88857376928db75
Olaf Delgado-Friedrichs <olaf.delgado@gmail.com>
2009-09-10 17:57:07 +1000

    Added some validations for users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,20 @@ class User < ActiveRecord::Base
   attr_accessible(:username, :email, :password, :password_confirmation,
                   :name, :address, :telephone, :fax)
 
+  # -- patterns for checking phone numbers
+  SEP   = /( |-)?/
+  WORLD = / \+\d{1,3} #{SEP} (\d|\(\d\)) #{SEP} \d{1,5} /xo
+  AREA  = / \d{2,6} | \(\d{2,6}\) /xo
+  LINE  = / \d{2,4} #{SEP} \d{4} /xo
+  EXT   = /#{SEP}(ext|x)#{SEP}\d{1,5}/xo
+  PHONE = /\A (#{WORLD}|#{AREA}) #{SEP} #{LINE} #{EXT}? \Z/xo
+
+  validates_presence_of :name
+  validates_presence_of :address
+  validates_format_of   :telephone, :with => PHONE,
+                        :message => "should look like a phone number"
+  validates_format_of   :fax, :with => PHONE,
+                        :message => "should look like a phone number"
+
   has_many :studies
 end
