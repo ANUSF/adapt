8632bd43c1cce1ea5961c0a2c971ded7afd3fdeb
Olaf Delgado-Friedrichs <olaf.delgado@gmail.com>
2009-09-11 17:33:12 +1000

    Added a form builder.

--- app/models/study.rb
+++ app/models/study.rb
@@ -1,5 +1,18 @@
 class Study < ActiveRecord::Base
   belongs_to :user
 
+  validates_presence_of :title
+  validates_presence_of :abstract
+
   attr_accessible :name, :title, :abstract
+
+  def help_on(column)
+    case column.to_sym
+    when :name
+      "Here you can assign an optional abbreviated name to refer to this " +
+        "study by."
+    else
+      nil
+    end
+  end
 end
