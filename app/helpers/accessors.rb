2ab7070b6256f6d3826fa3ae8d0d8e1e26aa9e37
Olaf Delgado-Friedrichs <olaf.delgado@gmail.com>
2009-10-13 14:37:43 +1100

    Uses simple text fields for data input in study model.

--- app/helpers/accessors.rb
+++ app/helpers/accessors.rb
@@ -1,7 +1,6 @@
 module Accessors
   def json_accessors(name)
     define_method("#{name}=") do |value|
-      puts "#{name} = #{value.inspect}"
       fields = ActiveSupport::JSON.decode(read_attribute :additional_metadata)
       fields[name.to_s] = value
       write_attribute :additional_metadata, fields.to_json
