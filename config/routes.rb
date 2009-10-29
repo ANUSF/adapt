9c58561e2f9aa581f012d32feff63aefe65dfe65
Olaf Delgado-Friedrichs <olaf.delgado@gmail.com>
2009-10-29 17:54:51 +1100

    Prepared for file upload page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :study_acknowledgements
   map.resources :user_sessions
   map.resources :users
   map.resources :studies
   map.resources :study_data
+  map.resources :study_acknowledgements
+  map.resources :study_files
 
   map.login "login", :controller => 'user_sessions', :action => 'new'
   map.logout "logout", :controller => 'user_sessions', :action => 'destroy'
