7f550b84b43093ca9848d4175eba2b36f176ea2a
Olaf Delgado-Friedrichs <olaf.delgado-friedrichs@anu.edu.au>
2009-11-10 17:16:47 +1100

    Cleaned up handling of environment variables and such.

--- config/environment.rb
+++ config/environment.rb
@@ -1,12 +1,14 @@
-require 'java'
-System = java.lang.System
-for key in %w{RAILS_IS_LOCAL ADAPT_ASSET_PATH}
-  ENV[key] ||= System.getProperty(key)
-end
-
 RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
 
 require File.join(File.dirname(__FILE__), 'boot')
+
+# -- read some missing environment variables from the Java system properties
+require 'java'
+System = java.lang.System
+for key in %w{ADAPT_IS_LOCAL ADAPT_DB_ADAPTER ADAPT_DB_PATH ADAPT_ASSET_PATH}
+  ENV[key] ||= System.getProperty(key)
+end
+ENV['HOME'] ||= System.getProperty('user.home')
 
 Rails::Initializer.run do |config|
   config.gem "haml"
@@ -24,6 +26,7 @@ Rails::Initializer.run do |config|
     # -- automatically migrate the database on startup
     migration_path = RAILS_ROOT + "/db/migrate"
     ActiveRecord::Migrator.migrate(migration_path)
+
     # -- db store for OpenID has problems with some adapters
     OpenIdAuthentication.store = :file
   end
