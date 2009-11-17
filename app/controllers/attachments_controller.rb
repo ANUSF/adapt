8890a6ce1207c5f5e1f57ded34395797b0b60b96
Olaf Delgado-Friedrichs <olaf.delgado-friedrichs@anu.edu.au>
2009-11-17 15:31:37 +1100

    Implemented a whitelist-style authorization filter for all controllers.

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,12 +1,16 @@
 class AttachmentsController < ApplicationController
+  permit :new, :create, :if => :current_user_owns_study
+  permit :show, :download, :edit, :update, :destroy,
+         :if => :current_user_owns_attachment
+
+  before_filter :find_study, :only => [ :new, :create ]
+  before_filter :find_attachment, :except => [ :new, :create ]
+
   def new
-    @study = Study.find(params[:study_id])
     @attachment = @study.attachments.new
   end
   
   def create
-    @study = Study.find(params[:study_id])
-
     if params[:result] == "Cancel"
       flash[:notice] = "Attachment upload cancelled."
       redirect_to @study
@@ -22,20 +26,16 @@ class AttachmentsController < ApplicationController
   end
   
   def show
-    @attachment = Attachment.find(params[:id])
   end
 
   def download
-    @attachment = Attachment.find(params[:id])
     send_data(@attachment.data, :filename => @attachment.name)
   end
 
   def edit
-    @attachment = Attachment.find(params[:id])
   end
   
   def update
-    @attachment = Attachment.find(params[:id])
     if params[:result] == "Cancel"
       flash[:notice] = "Attachment update cancelled."
       redirect_to @attachment
@@ -50,9 +50,29 @@ class AttachmentsController < ApplicationController
   end
 
   def destroy
-    @attachment = Attachment.find(params[:id])
     @attachment.destroy
     flash[:notice] = "Successfully deleted attachment '#{@attachment.name}'"
     redirect_to @attachment.study
+  end
+
+  private
+
+  def find_study
+    return @study if defined? @study
+    @study = logged_in && current_user.studies.find_by_id(params[:study_id])
+  end
+
+  def current_user_owns_study
+    not find_study.nil?
+  end
+
+  def find_attachment
+    return @attachment if defined? @attachment
+    file = Attachment.find_by_id(params[:id])
+    @attachment = file && (file.study.user == current_user ? file : nil)
+  end
+
+  def current_user_owns_attachment
+    not find_attachment.nil?
   end
 end
