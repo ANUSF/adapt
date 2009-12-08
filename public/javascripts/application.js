b20a4dad0e502a20250db814d9cdd6f01486e8c5
Olaf Delgado-Friedrichs <olaf.delgado-friedrichs@anu.edu.au>
2009-12-08 15:40:28 +1100

    Made sections of study edit form collapsible.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,37 @@
 (function() {
     function onload(context) {
 	fixPage();
+
+	// -- deals with collapsible page content
+	jQuery('.collapsible', context).hide().before(
+          '<a href="" class="toggle noprint">[+]</a>' +
+          '<div class="proxy">&hellip;</div>');
+	jQuery('.toggle', context).click(function() {
+		var link = jQuery(this);
+		var placeholder = link.next();
+		var content = link.next().next();
+		if (content.is(':visible')) {
+		    content.hide();
+		    placeholder.show();
+		    link.html('[+]');
+		} else {
+		    content.show();
+		    placeholder.hide();
+		    link.html('[&ndash;]');
+		}
+		return false;
+	});
+	jQuery('.toggle', context).hover(function() {
+		jQuery(this).next().addClass('toggle-show');
+		jQuery(this).next().next().addClass('toggle-hide');
+		return false;
+	    }, function() {
+		jQuery(this).next().removeClass('toggle-show');
+		jQuery(this).next().next().removeClass('toggle-hide');
+	    });
+		
     }
+
     function fixPage() {
 	jQuery('table.zebra')
 	    .find('tr:nth-child(odd)').removeClass('odd').addClass('even').end()
