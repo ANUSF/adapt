0fda5e8bb2129eaf2b3300b59803e8da0e3bfcd2
Olaf Delgado-Friedrichs <olaf.delgado-friedrichs@anu.edu.au>
2009-12-21 17:39:38 +1100

    Users are identified by their username; the rest of the OpenID URI is ignored.

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -11,18 +11,16 @@ class UserSessionsController < ApplicationController
     openID_url = params[:login] ? OPENID_SERVER + params[:login] : nil
 
     if bypass_openid
-      login_as openID_url, "Demo mode: ASSDA ID was not checked."
+      login_as openID_url, :message => "Demo mode: ASSDA ID was not checked."
     else
-      sreg = ["email", "fullname"]
-      #sreg = []
-      authenticate_with_open_id(openID_url, :optional => sreg) do
-        |result, ident_url, profile|
-
+      args = [openID_url, { :optional => ["email", "fullname"] }]
+      authenticate_with_open_id(*args) do |result, ident_url, profile|
         if result.status == :successful
           Rails.logger.info(profile.inspect)
-          login_as ident_url
+          login_as ident_url, :profile => profile
         else
-          failed_login result.message
+          flash.now[:error] = result.message
+          render :action => 'new'
         end
       end
     end
@@ -36,31 +34,24 @@ class UserSessionsController < ApplicationController
 
   private
 
-  def login_as(ident_url, message = nil)
-    user = User.find_by_openid_identifier(ident_url)
-    if user.nil?
-      login_name = ident_url.sub(/^#{OPENID_SERVER}/, '')
-      name = case login_name
-             when /([a-z]+)\.([a-z]+)/i
-               "#{$1.capitalize} #{$2.capitalize}"
-             else
-               login_name
-             end
-      user = User.new(:username => login_name, :name => name)
+  def login_as(ident_url, options = {})
+    login_name = ident_url.sub(/^#{OPENID_SERVER}/, '')
+    user = User.find_or_create_by_username(login_name)
+    unless user.nil?
+      profile = options[:profile] || {}
+      user.role  = "contributor"       if user.role.blank?
+      user.name  = profile["fullname"] unless profile["fullname"].blank?
+      user.email = profile["email"]    unless profile["email"].blank?
       user.openid_identifier = ident_url
-      user.role = "contributor"
       user.save!
     end
+
     if user && login(user)
-      flash[:notice] = message || "Login successful."
+      flash[:notice] = options[:message] || "Login successful."
       redirect_to studies_url
     else
-      failed_login
+      flash.now[:error] = "Sorry, something went wrong. Please try again later!"
+      render :action => 'new'
     end
-  end
-
-  def failed_login(message = nil)
-    flash.now[:error] = message || "Login failed."
-    render :action => 'new'
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091126051223) do
+ActiveRecord::Schema.define(:version => 20091203011534) do
 
   create_table "attachments", :force => true do |t|
     t.integer  "study_id"
