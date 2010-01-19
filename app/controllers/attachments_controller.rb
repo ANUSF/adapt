3044e9173dd635a49e733706ff9876af587e3cf3
Olaf Delgado-Friedrichs <olaf.delgado-friedrichs@anu.edu.au>
2010-01-19 16:25:00 +1100

    attachment controller: added documentation.

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,30 +1,43 @@
 class AttachmentsController < ApplicationController
+  # ----------------------------------------------------------------------------
+  # Authorization and other filtering.
+  # ----------------------------------------------------------------------------
+
+  # -- find referenced resources before performing authorization
   before_authorization_filter :find_study, :only => [ :new, :create ]
   before_authorization_filter :find_attachment, :except => [ :new, :create ]
 
+  # -- declare access permissions via the 'verboten' plugin
   permit :show, :download, :if => :may_view
   permit :new, :create, :edit, :update, :destroy, :if => :may_edit
 
   private
 
+  # Finds the study with the id in the 'study_id' parameter.
   def find_study
     @study = Study.find_by_id(params[:study_id])
   end
 
+  # Finds the attachment with the given id and the study it belongs to.
   def find_attachment
     @attachment = Attachment.find_by_id(params[:id])
     @study = @attachment.study if @attachment
   end
 
+  # Whether the current user may view the referenced study.
   def may_view
     @study and @study.can_be_viewed_by current_user
   end
 
+  # Whether the current user may edit the referenced study.
   def may_edit
     @study and @study.can_be_edited_by current_user
   end
 
-
+  # ----------------------------------------------------------------------------
+  # The standard actions this controller implements. All action code relies on
+  # the @study and @attachment variables having been set by before filters.
+  # ----------------------------------------------------------------------------
   public
 
   def new
@@ -49,10 +62,6 @@ class AttachmentsController < ApplicationController
   def show
   end
 
-  def download
-    send_data(@attachment.data, :filename => @attachment.name)
-  end
-
   def edit
   end
   
@@ -74,5 +83,14 @@ class AttachmentsController < ApplicationController
     @attachment.destroy
     flash[:notice] = "Successfully deleted attachment '#{@attachment.name}'"
     redirect_to @attachment.study
+  end
+
+  # ----------------------------------------------------------------------------
+  # Additional actions this controller provides.
+  # ----------------------------------------------------------------------------
+
+  # This custom action provides a file download for the referenced attachment.
+  def download
+    send_data(@attachment.data, :filename => @attachment.name)
   end
 end
