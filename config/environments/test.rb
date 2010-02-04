f8f2e4cc4f6fc42aa877a30905f6b1ab118bde08
Olaf Delgado-Friedrichs <olaf.delgado-friedrichs@anu.edu.au>
2010-02-04 17:19:22 +1100

    Generated rspec support files.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -28,3 +28,5 @@ config.gem "cucumber", :lib => false
 config.gem "machinist", :source => "http://gemcutter.org"
 config.gem "faker"
 config.gem "pickle", :lib => false
+
+  config.gem 'rspec-rails', :version => '>= 1.3.2', :lib => false unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec-rails'))
