0f431efba31dab0d19526fb73b2821ef1bd4bced
Olaf Delgado-Friedrichs <olaf.delgado-friedrichs@anu.edu.au>
2010-02-26 18:07:49 +1100

    Started to fill in DDI xml.

--- app/models/study_ddi.rb
+++ app/models/study_ddi.rb
@@ -1,18 +1,55 @@
-require 'nokogiri'
-
 module StudyDDI
   def to_ddi
     builder = Nokogiri::XML::Builder.new do |xml|
       xml.codeBook(:version => "1.2.2") {
-        xml.docDscr {
-        }
-        xml.stdyDscr {
-        }
-        xml.fileDscr {
-        }
-        xml.dataDscr {
-        }
+        docDscr(xml)
+        stdyDscr(xml)
+        fileDscr(xml)
+        dataDscr(xml)
       }
     end
+  end
+
+  private
+  def docDscr(xml)
+    xml.docDscr  { 
+      xml.citation {
+        xml.titlStmt {
+          xml.titl title
+        }
+        xml.prodStmt
+        xml.verStmt
+      }
+    }
+  end
+  
+  def stdyDscr(xml)
+    xml.stdyDscr {
+      xml.citation {
+        xml.titlStmt {
+          xml.titl title
+        }
+        xml.rspStmt
+        xml.prodStmt
+        xml.distStmt
+        xml.serStmt
+        xml.verStmt
+        xml.biblCit
+      }
+      stdyInfo(xml)
+    }
+  end
+
+  def stdyInfo(xml)
+    xml.stdyInfo {
+      xml.subject
+      xml.abstract abstract
+    }
+  end
+
+  def fileDscr(xml)
+  end
+
+  def dataDscr(xml)
   end
 end
