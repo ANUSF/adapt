05830e0a5051751930b497389c1e4945a4643944
Olaf Delgado-Friedrichs <olaf.delgado-friedrichs@anu.edu.au>
2010-03-09 15:15:22 +1100

    Refactored the code for collapsibles.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,41 +1,41 @@
 (function() {
+    function toggle(item) {
+	var link = jQuery(item);
+	var placeholder = link.parent().find('.proxy');
+	var content = link.parent().find('.collapsible');
+	var field = link.parent().find('input.state');
+	if (content.is(':visible')) {
+	    content.hide();
+	    placeholder.show();
+	    link.html('[+]');
+	    field.attr('value', 'false');
+	} else {
+	    content.show();
+	    placeholder.hide();
+	    link.html('[&ndash;]');
+	    field.attr('value', 'true');
+	}
+    }
+
     function onload(context) {
 	fixPage();
 
 	// -- deals with collapsible page content
-	jQuery('.collapsible', context).hide().wrap("<div></div>").before(
-          '<a href="" class="toggle noprint">[+]</a>' +
-          '<div class="proxy">&hellip;</div><div class="clear"/>' +
-	  '<input type = "hidden" class = "state" name = "" value = "">' );
-	jQuery('.collapsible.start-open', context).each(function() {
+	jQuery('.collapsible', context).wrap("<div/>").each(
+	    function() {
 		var content = jQuery(this);
-		var placeholder = content.parent().find('.proxy');
-		var link = content.parent().find('.toggle');
-		var field = content.parent().find('input.state');
-
-		content.show();
-		placeholder.hide();
-		link.html('[&ndash;]');
-		field.attr('name', content.attr('id'));
-		field.attr('value', 'true');
-	});
+		content.before('<div class="proxy">&hellip;</div>' +
+			       '<div class="clear"/>' +
+			       '<input type="hidden" class="state" name=' +
+			       '"show-'+content.attr('id')+'"/>');
+		var link  = jQuery('<a href="" class="toggle noprint"/>');
+		content.parent().prepend(link);
+		if (content.hasClass('start-open')) content.hide();
+		toggle(link);
+	    }
+	);
 	jQuery('.toggle', context).click(function() {
-		var link = jQuery(this);
-		var placeholder = link.parent().find('.proxy');
-		var content = link.parent().find('.collapsible');
-		var field = link.parent().find('input.state');
-		if (content.is(':visible')) {
-		    content.hide();
-		    placeholder.show();
-		    link.html('[+]');
-		    field.attr('value', 'false');
-		} else {
-		    content.show();
-		    placeholder.hide();
-		    link.html('[&ndash;]');
-		    field.attr('name', content.attr('id'));
-		    field.attr('value', 'true');
-		}
+		toggle(this);
 		return false;
 	});
 	jQuery('.toggle', context).hover(function() {
