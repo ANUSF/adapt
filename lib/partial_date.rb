d2371d148a002a541335e6e0cce79a51393ece47
Olaf Delgado-Friedrichs <olaf.delgado-friedrichs@anu.edu.au>
2010-03-18 16:32:39 +1100

    PartialDate: finished date parser; some refactoring.

--- lib/partial_date.rb
+++ lib/partial_date.rb
@@ -2,20 +2,14 @@ require 'delegate'
 
 class PartialDate
   class Year < DelegateClass(Integer)
-    def initialize(val)
-      case val
-      when Integer
-        raise "There was no year 0." if val == 0
-      when Year
-        val = val.to_i
-      else
-        raise "Expected an integer or year; got #{val.class}."
+    def initialize(x)
+      case x
+      when 0       then raise "There was no year 0."
+      when Year    then val = x.to_i
+      when Integer then val = x
+      else              raise "Expected an integer or year; got #{x.class}."
       end
       super(val)
-    end
-
-    def is_leap_year?
-      self % 4 == 0 and (self < 1753 or self % 100 != 0 or self % 400 == 0)
     end
 
     def to_s
@@ -24,18 +18,6 @@ class PartialDate
   end
 
   class Month < DelegateClass(Integer)
-    DAYS_IN_MONTH = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    MONTH = [ nil,
-              "January", "February", "March", "April", "May", "June",
-              "July", "August", "September", "October", "November", "December" ]
-
-    MONTH_BY_NAME = {
-      "Jan" =>  1, "Feb" =>  2, "Mar" =>  3,
-      "Apr" =>  4, "May" =>  5, "Jun" =>  6,
-      "Jul" =>  7, "Aug" =>  8, "Sep" =>  9,
-      "Oct" => 10, "Nov" => 11, "Dec" => 12
-    }
-
     def initialize(val)
       case val
       when Integer
@@ -53,11 +35,7 @@ class PartialDate
     end
 
     def number_of_days(year = nil)
-      if self == 2
-        if year and Year.new(year).is_leap_year? then 29 else 28 end
-      else
-        DAYS_IN_MONTH[self]
-      end
+      DAYS_IN_MONTH[self]
     end
 
     def short_name
@@ -67,6 +45,24 @@ class PartialDate
     def long_name
       MONTH[self]
     end
+
+    def to_s
+      if (1..9).include? self then "0" else "" end + self.to_i.to_s
+    end
+
+    private
+
+    DAYS_IN_MONTH = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    MONTH = [ nil,
+              "January", "February", "March", "April", "May", "June",
+              "July", "August", "September", "October", "November", "December" ]
+
+    MONTH_BY_NAME = {
+      "Jan" =>  1, "Feb" =>  2, "Mar" =>  3,
+      "Apr" =>  4, "May" =>  5, "Jun" =>  6,
+      "Jul" =>  7, "Aug" =>  8, "Sep" =>  9,
+      "Oct" => 10, "Nov" => 11, "Dec" => 12
+    }
   end
 
   class Day < DelegateClass(Integer)
@@ -80,6 +76,10 @@ class PartialDate
         raise "Expected integer or day; got #{val.class}."
       end
       super(val)
+    end
+
+    def to_s
+      if (1..9).include? self then "0" else "" end + self.to_i.to_s
     end
   end
 
@@ -102,13 +102,10 @@ class PartialDate
         end
 
     @year = Year.new x[0]
-    if x.size >= 2
-      @month = Month.new x[1]
-      if x.size >= 3
-        assert_legal_day(x[2])
-        @day = Day.new x[2]
-      end
-    end
+    @month = Month.new(x[1]) if x[1]
+    @day   = Day.new(x[2])   if x[2]
+    assert_legal_date
+    freeze
   end
 
   def to_s
@@ -125,20 +122,96 @@ class PartialDate
 
   private
 
-  YEAR_ANY   = /\A -?\d+ \z/xo
-  YEAR_LONG  = /\A -?\d{4} \z/xo
-  YEAR_SHORT = /\A \d\d \z/xo
-  DAY        = /\A (?: 0?[1-9] | [1-2][0-9] | 3[0-1] ) \z/xo
-  MONTH_INT  = /\A (?: 0?[1-9] | 1[0-2] ) \z/xo
-  MONTH_STR  = /\A [a-z]{3,} \z/ixo
-  MONTH      = / #{MONTH_INT} | #{MONTH_STR} /ixo
-  SEPARATOR  = /\A (?: [-.\/] | \,?\s+ ) \z/xo
+  def assert_legal_date
+    #TODO support other dates for adoption of Gregorian calendar
+
+    leap = year % 4 == 0 and (year < 1753 or year % 100 != 0 or year % 400 == 0)
+
+    if day
+      if month
+        if month == 2 and day == 29 and not leap
+          raise "#{year} is not a leap year."
+        elsif day > month.number_of_days
+          raise "#{month.long_name} does not have #{day} days."
+        end
+      else
+        raise "Can't have a day without a month." unless month
+      end
+      if year == 1752 and month == 9 and (3..13).include? day
+        raise "Give us our eleven days!"
+      end
+    end
+  end
+
+  YEAR      = /\A -?\d+ \z/xo
+  DAY       = /\A (?: 0?[1-9] | [1-2][0-9] | 3[0-1] ) \z/xo
+  MONTH_INT = /\A (?: 0?[1-9] | 1[0-2] ) \z/xo
+  MONTH_STR = /\A [a-z]{3,} \z/ixo
+  MONTH     = / #{MONTH_INT} | #{MONTH_STR} /ixo
+  SEPARATOR = /\A (?: [-.\/] | \,?\s+ ) \z/xo
+
+  def self.parse(s)
+    date_string = s.strip
+    a = date_string.split /\b/
+    case a.size
+    when 1
+      [resolve_year(a[0])]
+    when 3
+      assert_format(a[1], SEPARATOR, "separator")
+      if a[0] =~ MONTH and not a[2] =~ MONTH_STR
+        resolve(date_string, a[0], a[2], nil)
+      elsif a[2] =~ MONTH
+        resolve(date_string, a[2], a[0], nil)
+      else
+        unknown date_string
+      end
+    when 5
+      assert_format(a[1], SEPARATOR, "separator")
+      assert_format(a[3], SEPARATOR, "separator")
+      if a[1] == a[3] and date_string =~ /\A\d{4}.\d\d.\d\d\z/
+        [resolve_year(a[0]), resolve_month(a[2]), resolve_day(a[4])]
+      elsif a[0] =~ MONTH and not (a[2] =~ MONTH_STR or a[4] =~ MONTH_STR)
+        resolve(date_string, a[0], a[2], a[4])
+      elsif a[2] =~ MONTH and not a[4] =~ MONTH_STR
+        resolve(date_string, a[2], a[4], a[0])
+      elsif a[4] =~ MONTH
+        resolve(date_string, a[4], a[0], a[2])
+      else
+        unknown date_string
+      end
+    else
+      unknown date_string
+    end
+  end
+
+  def self.resolve(date_string, month, a, b)
+    if month =~ MONTH_STR or not (a =~ MONTH or b =~ MONTH)
+      if b.nil?
+        [resolve_year(a), resolve_month(month)]
+      elsif not b =~ DAY
+        [resolve_year(b), resolve_month(month), resolve_day(a)]
+      elsif not a =~ DAY
+        [resolve_year(a), resolve_month(month), resolve_day(b)]
+      else
+        ambiguous date_string
+      end
+    else
+      ambiguous date_string
+    end
+  end
+
+  def self.resolve_day(s)
+    assert_format(s, DAY, "day")
+    s.to_i
+  end
 
   def self.resolve_month(s)
+    assert_format(s, MONTH, "month")
     Month.new(if s =~ /\d+/ then s.to_i else s end)
   end
 
   def self.resolve_year(s)
+    assert_format(s, YEAR, "year")
     year = s.to_i
     if s.size == 2
       now = Date.today.year
@@ -149,53 +222,15 @@ class PartialDate
     end
   end
 
-  def self.parse(s)
-    date_string = s.strip
-    parts = date_string.split /\b/
-    case parts.size
-    when 1
-      assert_format(parts[0], YEAR_ANY, "year")
-      [parts[0].to_i]
-    when 3
-      assert_format(parts[1], SEPARATOR, "separator")
-      if parts[2] =~ MONTH
-        if parts[2] =~ MONTH_STR or not parts[0] =~ MONTH
-          assert_format(parts[0], YEAR_ANY, "year")
-          [resolve_year(parts[0]), resolve_month(parts[2])]
-        else
-          raise "Ambiguous date: '#{date_string}'."
-        end
-      elsif parts[0] =~ MONTH
-        assert_format(parts[2], YEAR_ANY, "year")
-        [resolve_year(parts[2]), resolve_month(parts[0])]
-      else
-        raise "Unknown date format: '#{date_string}'."
-      end
-    end
+  def self.assert_format(text, pattern, concept)
+    raise("Illegal format for #{concept}: '#{text}'.") unless text =~ pattern
   end
 
-  def self.assert_format(text, pattern, concept)
-    unless text =~ pattern
-      raise "Illegal format for #{concept}: '#{text}'."
-    end
+  def self.ambiguous(s)
+    raise "Ambiguous date (consider using 4-digit year or month name): '#{s}'."
   end
-  
-  def assert_legal_day(day)
-    if day
-      if month
-        if day > month.number_of_days(year)
-          if month == 2 and day == 29
-            raise "#{year} is not a leap year."
-          else
-            raise "#{month.long_name} does not have #{day} days."
-          end
-        end
-      else
-        raise "Can't have a day without a month." unless month
-      end
-      if year == 1752 and month == 9 and (3..13).include? day
-        raise "Give us our eleven days!"
-      end
-    end
+
+  def self.unknown(s)
+    raise "Unrecognized date format: '#{s}'."
   end
 end
