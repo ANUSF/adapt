61cddc1650317b68fa4c796cc30ee7876b413e54
Olaf Delgado-Friedrichs <olaf.delgado-friedrichs@anu.edu.au>
2010-03-25 16:34:25 +1100

    Using verboten as a gem now.

--- vendor/plugins/verboten/tasks/verboten_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :verboten do
-#   # Task goes here
-# end
--- vendor/plugins/verboten/test/verboten_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test/unit'
-
-class VerbotenTest < Test::Unit::TestCase
-  # Replace this with your real tests.
-  def test_this_plugin
-    flunk
-  end
-end
