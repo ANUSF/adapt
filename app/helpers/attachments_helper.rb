0cae545d156d97e1e68f6ea034f90cf4e56d041c
Olaf Delgado-Friedrichs <olaf.delgado-friedrichs@anu.edu.au>
2010-04-08 11:26:52 +1000

    Changed the formatting of attachment listing.

--- app/helpers/attachments_helper.rb
+++ app/helpers/attachments_helper.rb
@@ -1,2 +1,6 @@
 module AttachmentsHelper
+  def format_attachment(attachment)
+    link = link_to h(attachment.name), download_attachment_path(attachment)
+    format_text(attachment.description).sub(/<p>/, "<p>#{link} &mdash; ")
+  end
 end
