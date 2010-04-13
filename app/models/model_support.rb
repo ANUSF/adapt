394fdd57bcc74b9447a06ac61e97dc2d1c25106a
Olaf Delgado-Friedrichs <olaf.delgado-friedrichs@anu.edu.au>
2010-04-13 14:24:33 +1000

    Date validation falls back to Date.parse() if PartialDate.new() fails.

--- app/models/model_support.rb
+++ app/models/model_support.rb
@@ -24,7 +24,11 @@ module ModelSupport
   def parse_and_validate_date(attribute, value, min_year = 1000, max_year = 2999)
     result = false
     begin
-      date = PartialDate.new(value)
+      begin
+        date = PartialDate.new(value)
+      rescue
+        date = PartialDate.new(Date.parse(value))
+      end
     rescue => ex
       errors.add attribute, ex.message
     else
