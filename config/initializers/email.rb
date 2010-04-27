ebba979cf7a784dac88ed88bcb779b96909cf4a8
Olaf Delgado-Friedrichs <olaf.delgado-friedrichs@anu.edu.au>
2010-04-27 15:41:58 +1000

    ActionMailer uses delivery method :test unless in production mode.

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -1,7 +1,9 @@
 # Load mail configuration if not in test environment
 if RAILS_ENV != 'test'
   email_settings = YAML::load(File.open("#{RAILS_ROOT}/config/email.yml"))
-  unless email_settings[RAILS_ENV].nil?
+  if email_settings[RAILS_ENV]
     ActionMailer::Base.smtp_settings = email_settings[RAILS_ENV]
+  else
+    ActionMailer::Base.delivery_method = :test
   end
 end
