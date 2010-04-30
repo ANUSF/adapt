a34cad49501ae1ed681861755ae262620a7602b7
odf <olaf.delgado@gmail.com>
2010-05-01 00:22:10 +1000

    LazyEnumerable: added some more nifty methods.

--- lib/lazy_enumerable.rb
+++ lib/lazy_enumerable.rb
@@ -2,6 +2,7 @@ module LazyEnumerable
   include Enumerable
 
   def map(&block)
+    raise 'No block given.' unless block_given?
     Generator.new do |yielder|
       self.each do |value|
         yielder.yield(block.call(value))
@@ -9,9 +10,67 @@ module LazyEnumerable
     end
   end
 
+  def select(&block)
+    raise 'No block given.' unless block_given?
+    Generator.new do |yielder|
+      self.each do |value|
+        yielder.yield(value) if block.call(value)
+      end
+    end
+  end
+
+  def with_index
+    Generator.new do |yielder|
+      i = 0
+      self.each do |value|
+        yielder.yield([value, i])
+        i += 1
+      end
+    end
+  end
+
+  def take(n)
+    Generator.new do |yielder|
+      i = 0
+      self.each do |value|
+        if i < n
+          yielder.yield(value)
+          i += 1
+        else
+          return
+        end
+      end
+    end
+  end
+
+  def drop(n)
+    Generator.new do |yielder|
+      i = 0
+      self.each do |value|
+        if i >= n
+          yielder.yield(value)
+        else
+          i += 1
+        end
+      end
+    end
+  end
+
+  def fold(init, &block)
+    Generator.new do |yielder|
+      acc = init
+      self.each do |value|
+        acc = block.call(acc, value)
+        yielder.yield(acc)
+      end
+    end
+  end
+
   class Yielder
     def initialize(&block)
+      raise 'No block given.' unless block_given?
       @block = block
+      freeze
     end
 
     def yield(x)
@@ -23,16 +82,23 @@ module LazyEnumerable
     include LazyEnumerable
       
     def initialize(&code)
+      raise 'No block given.' unless block_given?
       @code = code
+      freeze
     end
 
     def each(&block)
-      @code.call(Yielder.new(&block))
+      if block_given?
+        @code.call(Yielder.new(&block))
+      else
+        self
+      end
     end
   end
 
   class Stream < Generator
     def initialize(first, &succ)
+      raise 'No block given.' unless block_given?
       super() do |yielder|
         this = first
         while this
