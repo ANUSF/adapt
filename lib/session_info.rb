0f15c056d29c42496592607cc8bfc172d582b3d2
Olaf Delgado-Friedrichs <olaf.delgado-friedrichs@anu.edu.au>
2010-05-06 16:07:20 +1000

    Mail is sent to current user when in demo mode.

---
+++ lib/session_info.rb
@@ -0,0 +1,17 @@
+module SessionInfo
+  def current_user
+    Thread.current[:user]
+  end
+
+  def self.current_user=(user)
+    Thread.current[:user] = user
+  end
+
+  def host
+    Thread.current[:host]
+  end
+
+  def self.host=(host)
+    Thread.current[:host] = host
+  end
+end
