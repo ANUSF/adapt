d099921eac153b95f99d6f2c97bd6fe5074b2a06
Olaf Delgado-Friedrichs <olaf.delgado-friedrichs@anu.edu.au>
2010-05-25 15:52:20 +1000

    Mail is sent upon a 500 status.

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -2,6 +2,7 @@ class UserMailer < ActionMailer::Base
   include SessionInfo
 
   ASSDA_EMAIL = "assda@anu.edu.au"
+  WEBMASTER = "olaf.delgado-friedrichs@anu.edu.au"
 
   def create!(*args)
     super
@@ -31,5 +32,13 @@ class UserMailer < ActionMailer::Base
     subject "ADAPT: A new study was assigned to you"
     sent_on Time.now
     body(:study => study, :url => edit_study_url(study, :host => request_host))
+  end
+
+  def error_notification(exception)
+    recipients WEBMASTER
+    from ASSDA_EMAIL
+    subject "ADAPT: Error notification"
+    sent_on Time.now
+    body(:exception => exception)
   end
 end
