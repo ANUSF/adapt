a92c5a2f970bb62b31497b0da67b239dcb84f6cb
Olaf Delgado-Friedrichs <olaf.delgado-friedrichs@anu.edu.au>
2010-06-10 17:33:52 +1000

    Fixed some small bugs.

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -1,9 +1,7 @@
 # Load mail configuration if not in test environment
-if RAILS_ENV != 'test'
-  email_settings = YAML::load(File.open("#{RAILS_ROOT}/config/email.yml"))
-  if email_settings[RAILS_ENV]
-    ActionMailer::Base.smtp_settings = email_settings[RAILS_ENV]
-  else
-    ActionMailer::Base.delivery_method = :test
-  end
+email_settings = YAML::load(File.open("#{RAILS_ROOT}/config/email.yml"))
+if email_settings[Rails.env]
+  ActionMailer::Base.smtp_settings = email_settings[RAILS_ENV]
+else
+  ActionMailer::Base.delivery_method = :test
 end
