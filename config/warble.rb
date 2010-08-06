58ad04e3c20640e726dcd26eb29186a5702eadab
Olaf Delgado-Friedrichs <olaf.delgado-friedrichs@anu.edu.au>
2010-08-06 12:25:19 +1000

    Cleaned up config/warble.rb

--- config/warble.rb
+++ config/warble.rb
@@ -1,8 +1,6 @@
 Warbler::Config.new do |config|
   config.dirs = %w(app config lib vendor script log tmp db/migrate)
 
-  #config.includes = FileList["db/db_bootstrap.sqlite3"]
-
   config.excludes = FileList["lib/tasks", "tmp/openids/*"]
 
   config.java_classes = FileList["java/bin/au/**/*"]
@@ -14,5 +12,4 @@ Warbler::Config.new do |config|
   config.gem_dependencies = true
 
   config.war_name = "adapt-#{Time.now.strftime("%Y%m%d%H%M%S")}"
-  #config.war_name = "adapt"
 end
