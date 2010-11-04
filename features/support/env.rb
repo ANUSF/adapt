9177b0d36b02479edafa0b165c80904f354a04f2
Olaf Delgado-Friedrichs <olaf.delgado-friedrichs@anu.edu.au>
2010-11-04 18:00:14 +1100

    Clean up the asset directory for each test.

--- features/support/env.rb
+++ features/support/env.rb
@@ -60,4 +60,15 @@ if defined?(ActiveRecord::Base)
   end
 end
 
+# Remove all stored mail deliveries
 Before { ActionMailer::Base.deliveries.clear }
+
+# Ensure a pristine assets directory for each scenario
+Before do
+  asset_path = ADAPT::CONFIG['adapt.asset.path']
+  %w{Archive Submission Temporary}.each do |s|
+    dirname = File.join(asset_path, s)
+    FileUtils.mkpath(dirname)
+    system("rm -rf #{dirname}/*")
+  end
+end
