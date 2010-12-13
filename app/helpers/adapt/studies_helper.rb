eaf3c1d72ffdb5714edda2d43d4247606453b616
Olaf Delgado-Friedrichs <olaf.delgado-friedrichs@anu.edu.au>
2010-12-13 12:26:24 +1100

    Replaced nested_fields with tabular_fields everywhere.

--- app/helpers/adapt/studies_helper.rb
+++ app/helpers/adapt/studies_helper.rb
@@ -104,46 +104,4 @@ module Adapt::StudiesHelper
       val.is_a?(Hash) ? (val[field] if val.has_key?(field)) : val
     end
   end
-
-  # Takes care of formtastic markup for repeatable and/or composite attributes
-  def nested_fields(form, attr, options = {})
-    obj = form.object
-    outer_options = { :class => obj.is_repeatable?(attr) ? 'repeatable' : '' }
-    title = I18n.t("formtastic.titles.#{attr}", :default => '')
-    outer_options[:name] = title unless title.blank?
-    outer_options[:name] = options[:name] if options.has_key?(:name)
-    if obj.subfields(attr).blank?
-      options[:label] = outer_options[:name] unless options.has_key? :label
-      outer_options.delete(:name)
-    end
-    fields_with_choices = options.delete(:choices_on) || []
-    
-    form.inputs outer_options do
-      concat(form.semantic_fields_for(:"#{attr}_items") do |f|
-        concat(f.inputs(:class => 'tabular') do
-          obj.subfields_for_nesting(attr).each do |field|
-            input_options = {}
-            input_options[:hint] =
-              I18n.t("formtastic.hints.#{obj.class.name.underscore}.#{attr}",
-                     :default => '')
-            for key in [:label, :required, :as] do
-              if options.has_key? key
-                val = options[key]
-                input_options[key] =
-                  val.is_a?(Hash) ? (val[field] if val.has_key?(field)) : val
-              end
-            end
-            extra_html = (options[:html_for] || {})[field] || {}
-            input_options[:input_html] = options[:input_html].merge extra_html
-            if fields_with_choices.include? field
-              input_options[:input_html][:'data-selection-id'] =
-                "#adapt-#{obj.subfields(attr) ? field : attr}-choices"
-            end
-            concat f.input(field, input_options)
-          end
-        end)
-      end)
-      concat(content_tag('li', form.errors_on(attr)))
-    end
-  end
 end
