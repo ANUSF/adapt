b0af990b92702c282d3f5dab16b126d723da9a92
Olaf Delgado-Friedrichs <olaf.delgado-friedrichs@anu.edu.au>
2011-01-04 16:25:03 +1100

    Pushed cleanup code for cucumber into a separate file.

--- features/support/env.rb
+++ features/support/env.rb
@@ -59,16 +59,3 @@ if defined?(ActiveRecord::Base)
   rescue LoadError => ignore_if_database_cleaner_not_present
   end
 end
-
-# Remove all stored mail deliveries
-Before { ActionMailer::Base.deliveries.clear }
-
-# Ensure a pristine assets directory for each scenario
-Before do
-  asset_path = ADAPT::CONFIG['adapt.asset.path']
-  %w{Archive Submission Temporary}.each do |s|
-    dirname = File.join(asset_path, s)
-    FileUtils.mkpath(dirname)
-    system("rm -rf #{dirname}/*")
-  end
-end
