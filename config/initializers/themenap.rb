1f5061376f12b84ac8ccc1dc6a40a3d3b0ebb88a
Olaf Delgado-Friedrichs <olaf.delgado-friedrichs@anu.edu.au>
2011-03-29 12:25:32 +1100

    Added an initialiser that configures themenap.

---
+++ config/initializers/themenap.rb
@@ -0,0 +1,14 @@
+Themenap::Config.server = 'http://testada'
+Themenap::Config.snippets =
+  [ { :css => 'title',
+      :text => '<%= yield :title %>' },
+    { :css => 'head',
+      :text => '<%= render "layouts/includes" %>',
+      :append => true },
+    { :css => 'article',
+      :text => '<%= render "layouts/body" %>' },
+    { :css => 'nav.subnav',
+      :text => '<%= render "layouts/links" %>' },
+    { :css => '.login',
+      :text => '<%= render "layouts/login" %>' } ]
+Themenap::Config.layout_name = 'ada'
