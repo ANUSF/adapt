3150c334423ee9ce0890191bc9a26654a44307a0
Olaf Delgado-Friedrichs <olaf.delgado-friedrichs@anu.edu.au>
2011-05-19 17:19:17 +1000

    Use test server at ANUSF for authentication.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < OpenidClient::SessionsController
-  DEFAULT_SERVER=ENV['ADA_OID'] || 'http://178.79.149.181:81'
+  DEFAULT_SERVER=ENV['ADA_OID'] || 'http://users-test.ada.edu.au'
 
   protected
 
