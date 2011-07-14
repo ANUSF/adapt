ed2953fdf5e7dbf9fd65d444a7cc926c8195e492
Olaf Delgado-Friedrichs <olaf.delgado-friedrichs@anu.edu.au>
2011-07-14 12:37:09 +1000

    Fixed a small attachment display problem in study summary.

--- app/helpers/adapt/attachments_helper.rb
+++ app/helpers/adapt/attachments_helper.rb
@@ -1,10 +1,11 @@
 module Adapt::AttachmentsHelper
   def format_attachment(attachment)
     link = link_to h(attachment.name), download_adapt_attachment_path(attachment)
-    if attachment.description.blank?
-      "<p>#{link} <em>(no description)</em></p>".html_safe
-    else
-      format_text(attachment.description).sub(/<p>/, "<p>#{link} &mdash; ")
-    end
+    t = if attachment.description.blank?
+          "<p>#{link} <em>(no description)</em></p>".html_safe
+        else
+          format_text(attachment.description).sub(/<p>/, "<p>#{link} &mdash; ")
+        end
+    t.html_safe
   end
 end
