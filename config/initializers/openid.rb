c755dc279f5c59a6baa9248981df760b3f77224c
Olaf Delgado-Friedrichs <olaf.delgado-friedrichs@anu.edu.au>
2011-08-03 14:40:47 +1000

    Upgraded to latest openid client plugin.

--- config/initializers/openid.rb
+++ config/initializers/openid.rb
@@ -1,4 +1,6 @@
 OpenidClient::Config.configure do |c|
   c.re_authenticate_after =
     ADAPT::CONFIG['ada.openid.expiration'].to_i || 15.minutes
+  c.client_state_key        = :_adapt_openid_client_state
+  c.session_controller_name = 'sessions'
 end
