3b79bc47591c1ad1f98580c9e4ecf30045b644b9
Olaf Delgado-Friedrichs <olaf.delgado-friedrichs@anu.edu.au>
2011-08-26 15:41:46 +1000

    Updated to openid_client 0.2.0

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,7 +25,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   # -- before filters
-  before_authorization_filter :update_authentication
+  before_authorization_filter :oid_update_authentication
   before_filter :store_session_info
   before_filter :set_asset_host
 
