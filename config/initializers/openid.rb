da6c77ac7cbd3b434dae3d6a266ed905db44f620
Olaf Delgado-Friedrichs <olaf.delgado-friedrichs@anu.edu.au>
2011-11-16 09:45:38 +1100

    Forced re-authentication happens after a week instead of 15 minutes.

--- config/initializers/openid.rb
+++ config/initializers/openid.rb
@@ -1,5 +1,5 @@
 OpenidClient::Config.configure do |c|
-  c.re_authenticate_after   = 15.minutes
+  c.re_authenticate_after   = 1.week
   c.client_state_key        = :_adapt_openid_client_state
   c.session_controller_name = 'sessions'
 end
