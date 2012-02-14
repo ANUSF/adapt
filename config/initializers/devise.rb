9893da22a7d8319bc00512001eea4a973309809b
odf <olaf.delgado@gmail.com>
2012-02-14 14:56:21 +1100

    Changed default email sender from assda@anu.edu.au to ada@anu.edu.au

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,7 +3,7 @@
 Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in DeviseMailer.
-  config.mailer_sender = "assda@anu.edu.au"
+  config.mailer_sender = "ada@anu.edu.au"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
