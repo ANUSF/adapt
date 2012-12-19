8caaee31922ea45d3ce484291ecfb6cac3ea0948
odf <olaf.delgado@gmail.com>
2012-12-19 15:31:01 +1100

    Added show and edit actions for DOI metadata entries

--- app/controllers/adapt/dois_controller.rb
+++ app/controllers/adapt/dois_controller.rb
@@ -3,7 +3,7 @@ class Adapt::DoisController < ApplicationController
 
   permit(:index, :new, :create, :show, :edit, :update, :destroy,
          :if => :user_is_archivist)
-  permit :send, :if => :user_is_archivist
+  permit :transmit, :if => :user_is_archivist
 
   private
 
@@ -27,17 +27,17 @@ class Adapt::DoisController < ApplicationController
 
   def create
     if params[:result] == "Cancel"
-      flash[:notice] = 'DOI creation cancelled.'
+      flash[:notice] = 'DOI metadata creation cancelled.'
       redirect_to root_url
     else
       @doi = Adapt::Doi.new(params[:adapt_doi])
 
       if @doi.save
-        flash[:notice] = 'DOI descriptor created.'
+        flash[:notice] = 'DOI metadata created.'
         redirect_to adapt_doi_url(@doi)
       else
         flash.now[:error] =
-          "DOI creation failed. Please correct the fields marked in red."
+          "DOI metadata creation failed. See fields marked in red."
         render :action => :new
       end
     end
@@ -50,6 +50,17 @@ class Adapt::DoisController < ApplicationController
   end
 
   def update
+    if params[:result] == "Cancel"
+      flash[:notice] = 'DOI metadata edit cancelled.'
+      redirect_to adapt_doi_url(@doi)
+    elsif @doi.update_attributes(params[:adapt_doi])
+      flash[:notice] = 'DOI metadata updated.'
+      redirect_to adapt_doi_url(@doi)
+    else
+      flash.now[:error] =
+        "DOI metadata creation failed. See fields marked in red."
+      render :action => :new
+    end
   end
 
   def destroy
@@ -58,5 +69,14 @@ class Adapt::DoisController < ApplicationController
   # === Extra action for this resource
 
   def transmit
+    if params[:result] == "Cancel"
+      flash[:notice] = 'DOI creation cancelled.'
+      redirect_to root_url
+    elsif params[:result] == "Edit"
+      redirect_to edit_adapt_doi_url(@doi)
+    else
+      flash[:notice] = 'DOI creation would have been initiated at this point.'
+      redirect_to adapt_doi_url(@doi)
+    end
   end
 end
